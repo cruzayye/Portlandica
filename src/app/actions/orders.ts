b1afa4e014de8abcf08dec1f36fcbb63bc747be0
cruzayye <johnnyc010392@gmail.com>
2026-03-06 09:50:11 -0800

    orders can be created

--- src/app/actions/orders.ts
+++ src/app/actions/orders.ts
@@ -1,12 +1,13 @@
 'use server'
 
-import { createClient } from '@/lib/supabase/server'
+import { createClient } from '@supabase/supabase-js'
 import type { NewOrder } from '@/types/orders'
 
 export async function createOrder(order: NewOrder) {
-  const supabase = await createClient()
-  console.log('createOrder response:', order) // Log the order data being sent to Supabase;
-
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
   const { data, error } = await supabase.from('orders').insert(order).select().single()
   if (error) throw new Error(error.message)
 
